0cfd281b8c62335379a84de834f1d04ca8beb254
Mehmet Emin BAŞOĞLU <mehmeteminbasoglu@hotmail.com>
2023-11-26 17:42:39 +0300

    feat(config.def.h): change font as ubuntu

--- config.def.h
+++ config.def.h
@@ -10,7 +10,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "Ubuntu:weight=bold:size=10:antialias=true:hinting=true",
+										"monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
